9ee054364b481b77f2b3814e70e463e78c1c61a5
Karim Said <inkriment@outlook.com>
2022-07-16 19:59:38 +0500

    Add models

---
+++ MoveisSite/Data/MovieCategory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoveisSite.Data
+{
+    public enum MovieCategory
+    {
+        Action = 1,
+        Comedy,
+        Drama,
+        Documentary
+    }
+}
---
+++ MoveisSite/Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using MoveisSite.Data;
+using System.ComponentModel.DataAnnotations;
+
+namespace MoveisSite.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public string ImageURL { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public MovieCategory MovieCategory { get; set; }
+    }
+}
