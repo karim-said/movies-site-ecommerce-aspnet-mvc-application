596fbdeafe1f7937754559cf7a1bb2b9b0609e2b
Karim Said <inkriment@outlook.com>
2022-07-18 15:18:59 +0500

    Adding   [ActorsController]

---
+++ MoveisSite/Controllers/ActorsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoveisSite.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoveisSite.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public ActorsController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var data = _context.Actors.ToList();
+
+            return View();
+        }
+    }
+}
