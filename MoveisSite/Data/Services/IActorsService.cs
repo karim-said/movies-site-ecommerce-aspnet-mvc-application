772a40bca76c90088e36bf670579c57f212b2e53
Karim Said <inkriment@outlook.com>
2022-07-20 23:35:09 +0500

    Adding IActorsService interface

---
+++ MoveisSite/Data/Services/IActorsService.cs
@@ -0,0 +1,18 @@
+﻿using MoveisSite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoveisSite.Data.Services
+{
+    public interface IActorsService
+    {
+        IEnumerable<Actor> GetAll();
+        Actor GetById(int id);
+        void Add(Actor actor);
+        Actor Update(int id, Actor newActor);
+        void Delete(int id);
+    }
+}
