86b27d7a9bca23c91e93671c61643a939daf1584
Karim Said <inkriment@outlook.com>
2022-07-23 23:32:40 +0500

    Getting item by id (Actor Details)

--- MoveisSite/Controllers/ActorsController.cs
+++ MoveisSite/Controllers/ActorsController.cs
@@ -15,7 +15,7 @@ namespace MoveisSite.Controllers
         }
         public async Task<IActionResult> Index()
         {
-            var allActors = await _service.GetAll();
+            var allActors = await _service.GetAllAsync();
 
             return View(allActors);
         }
@@ -23,7 +23,7 @@ namespace MoveisSite.Controllers
         //Get Actors/Create
         public async Task<IActionResult> Create()
         {
-            return View();
+            return  View();
         }
 
         [HttpPost]
@@ -34,8 +34,18 @@ namespace MoveisSite.Controllers
                 return View(actor);
             }
 
-            _service.Add(actor);
+            await _service.AddAsync(actor);
             return RedirectToAction(nameof(Index));
+        }
+
+        //Get Actors/Details/1
+        public async Task<IActionResult> Details(int id)
+        {
+            var actorDetails = await _service.GetByIdAsync(id);
+            if (actorDetails == null)
+                return View("Empty");
+
+            return View(actorDetails);
         }
     }
 }
--- MoveisSite/Data/Services/ActorsService.cs
+++ MoveisSite/Data/Services/ActorsService.cs
@@ -15,10 +15,10 @@ namespace MoveisSite.Data.Services
             _context = context;
         }
 
-        public void Add(Actor actor)
+        public async Task AddAsync(Actor actor)
         {
-            _context.Add(actor);
-            _context.SaveChanges();
+           await _context.Actors.AddAsync(actor);
+           await _context.SaveChangesAsync();
         }
 
         public void Delete(int id)
@@ -26,16 +26,17 @@ namespace MoveisSite.Data.Services
             throw new NotImplementedException();
         }
 
-        public async Task<IEnumerable<Actor>> GetAll()
+        public async Task<IEnumerable<Actor>> GetAllAsync()
         {
             var result = await _context.Actors.ToListAsync();
 
             return result;
         }
 
-        public Actor GetById(int id)
+        public async Task<Actor> GetByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            var result = await _context.Actors.FirstOrDefaultAsync(e => e.Id == id);
+            return result;
         }
 
         public Actor Update(int id, Actor newActor)
